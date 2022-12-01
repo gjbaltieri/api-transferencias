c45f4c4e7b571beeb8b55c5593e2dcce551b0d3b
Gustavo Jonas Baltieri <104153413+gjbaltieri@users.noreply.github.com>
2022-12-01 14:32:37 -0300

    change data-source config

--- build/main/data-source.js
+++ build/main/data-source.js
@@ -1,17 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appDataSource = void 0;
-require("reflect-metadata");
-require("dotenv/config");
-const typeorm_1 = require("typeorm");
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.appDataSource = void 0
+require('reflect-metadata')
+require('dotenv/config')
+const typeorm_1 = require('typeorm')
 exports.appDataSource = new typeorm_1.DataSource({
-    type: 'postgres',
-    port: 5432,
-    host: process.env.POSTGRES_HOST,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    synchronize: true,
-    migrations: [`./**/shared/typeorm/migrations/*.{ts,js}`],
-    entities: [`./**/infra/typeorm/entities/**.{ts,js}`]
-});
+  type: 'postgres',
+  port: 5432,
+  url: process.env.POSTGRES_URL,
+  synchronize: true,
+  ssl: true,
+  migrations: [`./build/shared/typeorm/migrations/*.{ts,js}`],
+  entities: [`./**/infra/typeorm/entities/**.{ts,js}`]
+})
